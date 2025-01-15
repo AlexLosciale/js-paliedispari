7616f4682185d997c02c7f0f5973f7aee758e273
AlexLosciale <losciale17@gmail.com>
2025-01-15 12:31:06 +0100

    add:start

---
+++ palindromo-js/script.js
@@ -0,0 +1,10 @@
+/*
+Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/*
+1-CHIEDERE ALL'UTENTE UNA PAROLA 
+2-CREARE UNA FUNZIONA PER INVERTIRE LA PAROLA AL CONTRARIO 
+3-CREARE UN IF DI VERIFICA, PER PARAGONARE LA PAROLA INIZALE CON QUELLA FINALE(INVERTITA)
+4-SPAMPARE IL RISULTATO FINALE
+*/
---
+++ pariOdispari-js/script.js
@@ -0,0 +1,13 @@
+/*
+Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) 
+per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando 
+una funzione) Dichiariamo chi ha vinto.
+*/
+
+/*
+1-FAR SCEGLIERE ALL'UTENTE UN NUMERO
+2-CREARE UN NUMERO RANDOM AL PC CON UNA FUNZIONE 
+3-SOMMA I DUE CON UNA FUNZIONE 
+  3.2-CREA UN IF PER STABILIRE SE IL NUMERO E PARI O DISPARI 
+4-SPAMAPRE IL RISULTATO DI CHI VINCE 
+*/
