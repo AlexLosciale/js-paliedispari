1f8b384540d999c0b349c6437bcc6bacb9100c4f
AlexLosciale <losciale17@gmail.com>
2025-01-15 16:51:17 +0100

    add:finish

--- pariOdispari-js/script.js
+++ pariOdispari-js/script.js
@@ -11,3 +11,34 @@ una funzione) Dichiariamo chi ha vinto.
   3.2-CREA UN IF PER STABILIRE SE IL NUMERO E PARI O DISPARI 
 4-SPAMAPRE IL RISULTATO DI CHI VINCE 
 */
+
+function creaNumeri() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
+function controlloPari(number) {
+  return number % 2 === 0;
+}
+
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+
+if (numeroUtente < 1, numeroUtente > 5) {
+  alert("Numero non valido");
+
+} else {
+  const numeroComputer = creaNumeri();
+  const numeriUniti = numeroUtente + numeroComputer;
+
+  let result;
+  if (controlloPari(numeriUniti)) {
+    result = "pari";
+  } else {
+    result = "dispari";
+  }
+
+  if (result = "pari") {
+    console.log("Il computer ha vinto");
+  } else {
+    console.log("Complimenti, hai vinto");
+  }
+}
